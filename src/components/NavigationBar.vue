9add0bc4b48abd9e5cdaef7b9ce787e45db101ba
bengreenbank <bengreenbank2001@icloud.com>
2022-05-18 19:38:46 +0100

    Create NavigationBar.vue component

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,5 +1,62 @@
 <template>
-  <header></header>
+  <header class="container font-sans">
+    <div
+      class="border-b-solid flex items-center justify-between border-b border-b-grey py-6"
+    >
+      <img
+        src="@/assets/img/icons/restaurant-menu.svg"
+        alt="Knife & Spoon"
+        width="48"
+        height="48"
+      />
+
+      <nav>
+        <ul class="flex items-center gap-6">
+          <li>
+            <a
+              class="decoration-blue decoration-2 underline-offset-4 transition-all hover:underline"
+              href="#"
+            >
+              Search
+            </a>
+          </li>
+
+          <li>
+            <a
+              class="decoration-blue decoration-2 underline-offset-4 transition-all hover:underline"
+              href="#"
+            >
+              Add
+            </a>
+          </li>
+
+          <li>
+            <a
+              class="decoration-blue decoration-2 underline-offset-4 transition-all hover:underline"
+              href="#"
+            >
+              Delete
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <a
+        class="flex items-center gap-2.5 rounded bg-blue px-3 py-2 text-sm text-white"
+        href="https://github.com/bengreenbank/restaurant-finder"
+        target="_blank"
+      >
+        View on GitHub
+
+        <img
+          src="@/assets/img/icons/open-in-new--white.svg"
+          width="14"
+          height="14"
+          alt="Open in new tab"
+        />
+      </a>
+    </div>
+  </header>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     colors: {
+      grey: '#D1D5DB',
       blue: '#5468ff',
+      white: '#ffffff',
     },
     fontFamily: {
       sans: '"Inter", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
+    },
+    container: {
+      padding: '1rem',
+      center: true,
     },
   },
   plugins: [],
