88b9598e8d09e6ab4085a86dc8a24aa7b96a89ae
bengreenbank <bengreenbank2001@icloud.com>
2022-05-18 19:05:26 +0100

    Import Inter front

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     colors: {
       blue: '#5468ff',
     },
+    fontFamily: {
+      sans: '"Inter", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
+    },
   },
   plugins: [],
 }
