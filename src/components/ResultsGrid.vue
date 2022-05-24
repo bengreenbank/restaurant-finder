0c1df1ae0e8cdc6f686de8cb19ee05510a487f42
bengreenbank <bengreenbank2001@icloud.com>
2022-05-24 19:55:50 +0100

    Add empty state to ResultsGrid.vue

--- src/components/ResultsGrid.vue
+++ src/components/ResultsGrid.vue
@@ -2,7 +2,10 @@
   <!-- This is the grid of results that displays. It uses our custom grid cards component and passes the items array to the for loop. -->
   <ais-hits class="col-span-9">
     <template v-slot="{ items }">
-      <ul class="grid gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+      <ul
+        v-if="items.length >= 1"
+        class="grid gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4"
+      >
         <GridCard
           :item="item"
           :index="index"
@@ -10,6 +13,14 @@
           :key="item.objectID"
         />
       </ul>
+
+      <p class="flex items-center justify-center text-center" v-else>
+        We're sorry, there are no restaurants matching your search criteria.
+
+        <br />
+
+        Please widen your search parameters.
+      </p>
     </template>
   </ais-hits>
 </template>
