9ecd0c664a5b7306ae2a5a5fa1e05d9d9ed8a0c7
bengreenbank <bengreenbank2001@icloud.com>
2022-05-25 07:43:00 +0100

    Fix bug when clicking pagination buttons

--- src/components/PaginationButtons.vue
+++ src/components/PaginationButtons.vue
@@ -14,28 +14,29 @@
     >
       <ul class="flex items-center gap-3 lg:gap-4">
         <!-- Button that returns to page 1 (actually page 0) -->
-        <li
-          v-if="!isFirstPage"
-          class="flex h-9 w-9 cursor-pointer items-center justify-center rounded bg-blue-light px-3 py-1 text-white"
-        >
-          <a :href="createURL(0)" @click.prevent="refine(0)"> ‹‹ </a>
+        <li v-if="!isFirstPage">
+          <a
+            :href="createURL(0)"
+            @click.prevent="refine(0)"
+            class="flex h-9 w-9 cursor-pointer items-center justify-center rounded bg-blue-light px-3 py-1 text-white"
+          >
+            ‹‹
+          </a>
         </li>
 
         <!-- Button that returns to the previous page. -->
-        <li
-          v-if="!isFirstPage"
-          class="flex h-9 w-9 cursor-pointer items-center justify-center rounded bg-blue text-white"
-        >
+        <li v-if="!isFirstPage">
           <a
             :href="createURL(currentRefinement - 1)"
             @click.prevent="refine(currentRefinement - 1)"
+            class="flex h-9 w-9 cursor-pointer items-center justify-center rounded bg-blue text-white"
           >
             ‹
           </a>
         </li>
 
         <!-- For every page within the (current range of pages) show a link to that page. -->
-        <li v-for="page in pages.slice(0, 3)" :key="page">
+        <li v-for="page in pages" :key="page">
           <a
             :href="createURL(page)"
             :class="{ 'font-semibold': page === currentRefinement }"
@@ -46,24 +47,23 @@
         </li>
 
         <!-- Button that advances to the next page. -->
-        <li
-          v-if="!isLastPage"
-          class="flex h-9 w-9 cursor-pointer items-center justify-center rounded bg-blue text-white"
-        >
+        <li v-if="!isLastPage">
           <a
             :href="createURL(currentRefinement + 1)"
             @click.prevent="refine(currentRefinement + 1)"
+            class="flex h-9 w-9 cursor-pointer items-center justify-center rounded bg-blue text-white"
           >
             ›
           </a>
         </li>
 
         <!-- Button that advances to the final page. -->
-        <li
-          v-if="!isLastPage"
-          class="flex h-9 w-9 cursor-pointer items-center justify-center rounded bg-blue-light px-3 py-1 text-white"
-        >
-          <a :href="createURL(nbPages)" @click.prevent="refine(nbPages)">
+        <li v-if="!isLastPage">
+          <a
+            :href="createURL(nbPages)"
+            @click.prevent="refine(nbPages)"
+            class="flex h-9 w-9 cursor-pointer items-center justify-center rounded bg-blue-light px-3 py-1 text-white"
+          >
             ››
           </a>
         </li>
