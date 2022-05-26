107cb3552c8170dc17f2fc4abbd089fe5603ce18
bengreenbank <bengreenbank2001@icloud.com>
2022-05-26 18:49:25 +0100

    Switch to env variables for API Keys

--- src/store/AlgoliaClient.js
+++ src/store/AlgoliaClient.js
@@ -5,10 +5,10 @@ export const useAlgoliaClientStore = defineStore('algoliaClientStore', {
   // State is like data in a Vue component. Here is where I define the data I need across multiple components.
   state: () => {
     return {
-      appId: 'DFY2HEF3K2',
-      apiKey: '9848dca93fbcb86c6ad80832b8ce7f75',
-      adminKey: 'e87e0e6ab2d84a8d6dcce4e391699038',
-      analyticsKey: '2c8393d48638ce3d3ad9c2f2f866f553',
+      appId: process.env.VUE_APP_ALGOLIA_APP_ID,
+      apiKey: process.env.VUE_APP_ALGOLIA_API_KEY,
+      adminKey: process.env.VUE_APP_ALGOLIA_ADMIN_KEY,
+      analyticsKey: process.env.VUE_APP_ALGOLIA_ANALYTICS_KEY,
     }
   },
   // The search client can be computed from the state of the API Keys above.
